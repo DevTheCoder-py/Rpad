cf6d417391b321d18beb9d04965a37656c5f3897
raihan <raihansunil@icloud.com>
2026-01-21 21:39:48 +0800

    Added ability to read notes; nano-like editor - WIP

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import json
 import os
+import time
+import curser
+from utils import *
+FP = "data.json"
+#Colors
+RED = "\033[31m"
+RESET = "\033[0m"
+# Sample in case i forget
+sample_note = {
+    "title": "test",
+    "lines": {
+        "1": "yo",
+        "2": "yor"
+    }
+}
+if os.path.exists(FP):
+    with open(FP, "r") as f:
+        notes = json.load(f)
+else:
+    notes = []
 
-file_path = "data.json"
 
-with open(file_path, "r") as f:
-    notes = json.load(f)
-new_note = [
-        {
-            "title": "sample",
-            "lines": {
-                1: "yo",
-                2: "yor"
-                }
-            }
-        ]
-print(notes)
-notes.append(new_note)
-with open(file_path, "w") as f:
-    json.dump(notes, f, indent=4)
+def AskUser():
+    mode = input("Notepad Mode?\n R or W;\n")
+    if mode == "R" or mode == "W":
+        return mode
+    else:
+        print("Please input valid mode")
+        clear_screen()
+        time.sleep(1)
+        AskUser()
+clear_screen()
+mode = AskUser()
+notes = loadNote(FP)
+RW(mode, notes)
+#todo planning to use stdscr to make nano like text editor
